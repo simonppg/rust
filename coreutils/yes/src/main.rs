41381d13089a01cd55d4c5bf36ce674eeb1e8bab
simonppg <simonppg@gmail.com>
2022-08-14 18:31:55 -0700

    feat(yes): show help and version, and print string from CLI arguments

--- coreutils/yes/src/main.rs
+++ coreutils/yes/src/main.rs
@@ -1,5 +1,48 @@
+extern crate getopts;
+use std::env;
+
 fn main() {
-    loop {
-        println!("y")
+    let args: Vec<String> = env::args().collect();
+    let mut opts = getopts::Options::new();
+
+    opts.optflag("h", "help", "show this information");
+    opts.optflag("v", "version", "command version");
+
+    let matches = opts.parse(&args[1..]).unwrap();
+
+    if matches.opt_present("h") { print_help(); return; }
+    if matches.opt_present("v") { print_version(); return; }
+
+    let mut message: String = "".to_string();
+
+    if args.len() > 1 {
+        for arg in args[1..].iter() {
+            message.insert_str(message.len(), arg);
+            message.insert_str(message.len(), " ");
+        }
     }
+
+    if message.len() > 1 {
+        loop {
+            println!("{}", message);
+        }
+    }
+    else {
+        loop {
+            println!("y")
+        }
+    }
+}
+
+fn print_help() {
+    println!("Usage: yes [STRING]...");
+    println!("or   : yes OPTIONS");
+    println!("Repeatedly output a line wiht all specified STRING(s), or 'y'");
+    println!("");
+    println!("       -h, --help    : display this help and exit");
+    println!("       -v, --version : output version information and exit");
+}
+
+fn print_version() {
+    println!("v0.1.0");
 }
